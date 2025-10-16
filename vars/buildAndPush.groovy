65536af63551bb95979d6d7a073ed9a4085fcc66
EC2 Default User <ec2-user@ip-10-0-1-168.us-east-2.compute.internal>
2025-10-16 05:12:05 +0000

    Dynamic service support

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,10 +1,13 @@
-def call(Map config) {
-    def service = config.service
-    def region = config.region ?: 'us-east-2'
-    def accountId = config.accountId ?: '442042505508'
+def call(Map config = [:]) {
+    // 1️⃣ Determine service name dynamically
+    def service = config.SERVICE_NAME ?: env.SERVICE_NAME
+    if (!service) { error "SERVICE_NAME is not defined!" }
+
+    def region = config.REGION ?: 'us-east-2'
+    def accountId = config.ACCOUNT_ID ?: '442042505508'
 
     stage("Docker Build & Push for ${service}") {
-        dir("src/${service}") {
+        dir("src/${service}") {   // Dockerfile should exist in src/<service>
             sh '''
                 docker system prune -f
                 docker container prune -f
--- vars/updateManifest.groovy
+++ vars/updateManifest.groovy
@@ -1,11 +1,13 @@
-def call(Map config) {
-    def service = config.service
-    def repoUrl = config.repoUrl
-    def yamlFile = config.yamlFile
-    def gitEmail = config.gitEmail
-    def gitUser = config.gitUser
-    def gitRepo = config.gitRepo
-    def gitTokenId = config.gitTokenId ?: 'my-git-pattoken'
+def call(Map config = [:]) {
+    def service = config.SERVICE_NAME ?: env.SERVICE_NAME
+    if (!service) { error "SERVICE_NAME is not defined!" }
+
+    def repoUrl = config.REPO_URL ?: "${config.ACCOUNT_ID ?: '442042505508'}.dkr.ecr.us-east-2.amazonaws.com/${service}"
+    def yamlFile = config.YAML_FILE ?: "${service}.yaml"
+    def gitEmail = config.GIT_EMAIL ?: "shrirang.patil1812@gmail.com"
+    def gitUser = config.GIT_USER ?: "DevByShrirang"
+    def gitRepo = config.GIT_REPO ?: "Microservices-E-Commerce-eks-project01"
+    def gitTokenId = config.GIT_TOKEN_ID ?: 'my-git-pattoken'
 
     stage("Update Manifest for ${service}") {
         dir('kubernetes-files') {
@@ -13,7 +15,6 @@ def call(Map config) {
                 sh """
                     git config user.email "${gitEmail}"
                     git config user.name "${gitUser}"
-
                     sed -i "s#image:.*#image: ${repoUrl}:${env.BUILD_NUMBER}#g" ${yamlFile}
                     git add .
                     git commit -m "Update ${service} image to version ${env.BUILD_NUMBER}"
